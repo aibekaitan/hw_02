917d004694de8a5982f5c55707b53d69851f2f54
Aibek <aibekaitan1@gmail.com>
2025-11-23 12:12:35 +0500

    Commit hw3 .env - 3

--- src/core/settings/settings.ts
+++ src/core/settings/settings.ts
@@ -1,9 +1,8 @@
-// @ts-ignore
-export const SETTINGS = {
-  DB_NAME: 'my_database',
-  MONGO_URL:
-    'mongodb+srv://aibekaitan1_db_user:-6fhA7-2ZqvddnE@cluster0.tm2ooam.mongodb.net/?appName=Cluster0',
+import 'dotenv/config';
 
-  PORT: 3000,
-  JWT_SECRET: 'some-secret',
+export const SETTINGS = {
+  DB_NAME: process.env.DB_NAME,
+  MONGO_URL: process.env.MONGO_URL,
+  PORT: Number(process.env.PORT) || 3000,
+  JWT_SECRET: process.env.JWT_SECRET,
 };
