15cf0d392effbfa75daafae88c11078a536432c1
Aibek <aibekaitan1@gmail.com>
2025-11-27 10:10:19 +0500

    Commit hw04 deleteMany - 11

--- src/testing/routers/testing.router.ts
+++ src/testing/routers/testing.router.ts
@@ -1,13 +1,16 @@
 import { Router, Request, Response } from 'express';
 import { db } from '../../db/in-memory.db';
 import { HttpStatus } from '../../core/types/http-statuses';
+import { blogsCollection, postsCollection } from '../../db/collections';
 
 export const testingRouter = Router({});
 
-testingRouter.delete('/all-data', (req: Request, res: Response) => {
+testingRouter.delete('/all-data', async (req: Request, res: Response) => {
   db.drivers = [];
   db.videos = [];
   db.posts = [];
   db.blogs = [];
+  await blogsCollection.deleteMany({});
+  await postsCollection.deleteMany({});
   res.sendStatus(HttpStatus.NoContent);
 });
